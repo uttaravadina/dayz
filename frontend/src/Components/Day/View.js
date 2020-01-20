ce4c76bfc1c25f90857e94fb82733687bce1aad6
Karen Ying <karenying7@gmail.com>
2020-01-20 15:37:27 -0500

    Add day colorbox

--- frontend/src/Components/Day/View.js
+++ frontend/src/Components/Day/View.js
@@ -29,11 +29,18 @@ class View extends React.Component {
         
         return (
             <>
-                <div>
+                <div className="day-wrapper">
                     <div className="day-bar">
                         <h3>{dayOfWeek}</h3>
                         <div className={todayActive}>
                             <h2>{date}</h2>
+                        </div>
+                    </div>
+                    <div className="day-content">
+                        <div className="day-colorbox-wrapper">
+                            <div className="day-colorbox">
+
+                            </div>
                         </div>
                     </div>
                 </div>
