d149d13ac0ffc2851ebaa82335eea282ca18b01d
Karen Ying <karenying7@gmail.com>
2020-02-23 15:48:18 -0500

    Add legend counts for day

--- frontend/src/Components/Day/Page.js
+++ frontend/src/Components/Day/Page.js
@@ -23,13 +23,19 @@ class Day extends React.Component {
         let today = new Date();
         today.setHours(0);
         let title = this.createTitle(today);
+        let counts = new Array(5).fill(0);
         getData(today)
             .then(output => {
+                if (output[0]) {
+                    let mood = output[0].mood;
+                    counts[mood]++;
+                }
                 this.setState({ 
                     day: today,
                     data: output[0],
                     title, 
                 });
+                this.props.setCounts(counts);
             });
     };
 
@@ -43,14 +49,21 @@ class Day extends React.Component {
         let currDay = this.state.day;
         let day = new Date(currDay.setDate(currDay.getDate() + direction));
         let today = new Date();
+        let counts = new Array(5).fill(0);
         today.setHours(0);
         if (day <= today) {
             getData(day)
             .then(output => {
+                if (output[0]) {
+                    let mood = output[0].mood;
+                    counts[mood]++;
+                }
                 this.setState({ 
                     data: output[0] 
                 });
+                this.props.setCounts(counts);
             });
+            
         } else {
             this.setState({data: null})
         }
